db2f54681b7f5b9157d43b753b1e840a0a4844ce
Gabriel <guzman.gabriel.92@gmail.com>
2023-03-08 15:45:50 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const saludar = () => {
+
+    console.log("EEEEEEEEEEEEHH");
+}
+
+
+console.log("AAAACAAAAAAAAAAAAAA");
+
+
+console.log("MIERDA!")
+
+console.log("TE VOY A ROMPER A PEDAZOS!")
+
+saludar();
